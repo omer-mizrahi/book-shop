703f734f95d0517583e705a0074112c793d6b5b6
Omer Mizrahi <omermizrahi@Omers-MacBook-Pro.local>
2021-05-26 18:48:49 +0300

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,12 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
-const _dirname = path.resolve()
+const __dirname = path.resolve()
 app.use(express.static(path.join(_dirname)))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(_dirname, '/frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }
 else {
     app.get('/', (req, res) => {
