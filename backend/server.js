6e72a917261700c8fd2439328dd8901c636d94d8
Omer Mizrahi <omermizrahi@Omers-MacBook-Pro.local>
2021-05-26 18:22:57 +0300

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,24 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Api is running')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+const _dirname = path.resolve()
+app.use('/uploads', express.static(path.join(_dirname, '/uploads')))
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(_dirname, '/frontend/build')))
+//     app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html')))
+// }
+// else {
+//     app.get('/', (req, res) => {
+//         res.send('Api is running')
+//     })
+
+// }
 
 app.use(notFound)
 
